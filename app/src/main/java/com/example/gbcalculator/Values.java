dbc3eb36af0b00972638cf2c83389d1032f77c80
anvlad25 <anvlad-2503@yandex.ru>
2021-06-08 21:55:08 +0300

    add logic

---
+++ app/src/main/java/com/example/gbcalculator/Values.java
@@ -0,0 +1,31 @@
+package com.example.gbcalculator;
+
+public class Values {
+    private double x = 0;
+    private double y = 0;
+    private String oper;
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public String getOper() {
+        return oper;
+    }
+
+    public void setOper(String oper) {
+        this.oper = oper;
+    }
+}
